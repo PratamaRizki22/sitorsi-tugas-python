a082423f6632bba32c8efa2538635ea766b457cb
PratamaRizki22 <rizkipurnomo914@gmail.com>
2023-08-17 04:26:34 +0700

    add sorting

--- sorting_algorithm.py
+++ sorting_algorithm.py
@@ -65,3 +65,34 @@ def merge_sort(my_list:list[int])->list[int]:
     right:list[int]=merge_sort(my_list[mid_index:])
 
     return merge(left, right)
+
+#Quick short
+
+def swap(my_list:list[int], index1:int,index2:int):
+    my_list[index1],my_list[index2]=my_list[index2],my_list[index1]
+
+
+def pivot(my_list:list[int], pivot_index:int, end_index:int)->int:
+    swap_index=pivot_index
+    for i in range(pivot_index+1,end_index+1):
+        if my_list[i]<my_list[pivot_index]:
+            swap_index+=1
+            swap(my_list, swap_index,i)
+
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+
+def quick_short_helper(my_list:list[int], left:int, right:int)->list[int]:
+    if left<right:
+        pivot_index=pivot(my_list, left, right)
+        quick_short_helper(my_list, left, pivot_index-1)
+        quick_short_helper(my_list, pivot_index+1,right)
+
+    return my_list
+
+def quick_short(my_list:list[int])->list[int]:
+    return quick_short_helper(my_list,0,len(my_list)-1)
+
+
+
